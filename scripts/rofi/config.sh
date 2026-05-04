9f6a3a11b86e6cf8eb7212aa9fd4e0af31204f17
vinescent <jshaurya21@gmail.com>
2026-05-04 09:37:56 -0400

    riced wlogout and other stuff

--- scripts/rofi/config.sh
+++ scripts/rofi/config.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-option=$(printf " Waybar\n Hyprland\n󰸉 Wallpaper\n Game Mode\n Color" | rofi -dmenu -p "Edit Config")
+option=$(printf " Waybar\n Hyprland\n󰸉 Wallpaper\n󰆿 Mouse Follow\n Game Mode\n Color" | rofi -dmenu -p "Edit Config")
 
 case "$option" in
    " Hyprland")   "$HOME/.config/scripts/rofi/hyprland.sh" ;;
    " Waybar") 	   "$HOME/.config/scripts/rofi/waybar.sh" ;;
    "󰸉 Wallpaper")  "$HOME/.config/scripts/rofi/monitor.sh" ;;
-   " Game Mode") "$HOME/.config/scripts/gamemode.sh" ;;
+   "󰆿 Mouse Follow")"$HOME/.config/scripts/mouse-follow.sh" ;;
+   " Game Mode")  "$HOME/.config/scripts/gamemode.sh" ;;
    " Color")      "$HOME/.config/scripts/rofi/color.sh" ;;
 esac
 
